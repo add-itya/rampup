ab9ade054cb1a04a2f8fef2099cd5d975cde8530
add-itya <jitheshaditya@gmail.com>
2022-09-23 18:09:52 -0400

    keras model

--- main.py
+++ main.py
@@ -14,3 +14,21 @@ model = Sequential()
 model.add(Dense(12, input_shape=(8,), activation='relu'))
 model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
+
+# compile the keras model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# fit the keras model on the dataset
+model.fit(X, y, epochs=150, batch_size=10, verbose=0)
+# make class predictions with the model
+predictions = (model.predict(X) > 0.5).astype(int)
+# summarize the first 5 cases
+for i in range(5):
+	print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], y[i]))
+
+'''# fit the keras model on the dataset
+model.fit(X, y, epochs=150, batch_size=10)
+
+# evaluate the keras model
+_, accuracy = model.evaluate(X, y)
+print('Accuracy: %.2f' % (accuracy*100))'''
