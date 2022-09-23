af6095601d55c2a860ab7bcea4aaa2c4971a5a0a
add-itya <jitheshaditya@gmail.com>
2022-09-23 17:40:23 -0400

    dataset and keras model

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ dataset = loadtxt('pima-indians-diabetes.csv', delimiter=',')
 X = dataset[:,0:8]
 y = dataset[:,8]
 
+# define the keras model
+model = Sequential()
+model.add(Dense(12, input_shape=(8,), activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
